79c5cce089c7b4ab0466b3e63a622e7774625e2c
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-09-27 22:41:23 +0530

    Disconnected helper library

---
+++ Employee_Helper.cs
@@ -0,0 +1,56 @@
+﻿using BusinessLogicLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DisconnectedBLL
+using Disconnected_DAL;
+
+namespace HelperLibrary
+{
+
+    public class EmployeeHelper
+    {
+        EmployeeDAL dal = null;
+        public EmployeeHelper()
+        {
+            dal = new EmployeeDAL();
+        }
+
+
+        public int RowCountEmployees()
+        {
+            return dal.RowCountEmployees();
+
+        }
+        public bool AddNewEmployee(Employee_BAL employee)
+        {
+            return dal.InsertEmployee(employee);
+        }
+        public Employee_BAL LocateEmployee(int empid)
+        {
+            return dal.FindEmployee(empid);
+        }
+
+        public List<Employee_BAL> EmployeeList()
+        {
+            return dal.ShowAllEmps();
+        }
+
+        public bool RemoveEmployeeData(int empid)
+        {
+            return dal.DeleteEmployee(empid);
+        }
+
+
+
+        public bool EditEmployeeData(int empid, Employee_BAL employee)
+        {
+            return dal.UpdateEmployee(empid, employee);
+        }
+
+
+
+    }
+}
